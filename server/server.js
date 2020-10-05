1ce725d131ca7e1901f4736570c4b21c608c9beb
Adrian Diaz <adrian93eh3@gmail.com>
2020-10-05 14:55:51 -0400

    deploying hopefully

--- server/server.js
+++ server/server.js
@@ -52,3 +52,14 @@ app.listen(PORT, () => {
 
 //-- Export to Tests ---------------------------------------------------------
 module.exports = app;
+
+// -- MONGO CONNECTION -------------------------------------------------------
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
