c1b71397ad0f03f24e63719a6e5a78ff3e3197ba
Adrian Diaz <adrian93eh3@gmail.com>
2020-10-08 10:24:46 -0400

    adding a backpack JS & JSON to populate items in the future as well as adding images for the JSON to be implemented soon

---
+++ client/src/components/Inventory/backpack.js
@@ -0,0 +1,25 @@
+import React from 'react';
+const backpack = require ('./backpack.json'); 
+
+class backpack extends React.Component {
+    render() {
+        return (
+            <div>
+                    <h1 className="backpack">Backpack</h1>
+                    <div className="parent">
+                        {backpack.backpack.map((item, key) => {
+                            return (
+                                <article className="child">
+                                    <img className="images" src={item.image}/>
+                                    <h3>{item.name}</h3>
+                                    <p>{item.description}</p>
+                                    <h5>{item.price}</h5>
+                                </article>
+                            )})}
+                    </div>
+            </div>
+        )
+    }
+}
+
+export default backpack;
