074d5eed25721cad5642ca6e5110a6727c551416
Aaron Stoiano <aaronstoiano@gmail.com>
2020-10-08 11:46:49 -0400

    pulling

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,24 +5,12 @@ import logo from './logo.svg';
 class HomePage extends Component {
   render() {
     return (
-      <div className='Home'>
-        <div className='row'>
-          <div className='col'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <p>
-              Edit <code>src/pages/Home.js</code> and save to reload.
-            </p>
-            <a
-              className='App-link'
-              href='https://reactjs.org'
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </div>
-        </div>
+      <body>
+      <div class="container">
+        
       </div>
+
+      </body>
     );
   }
 }
