67846fa20916dab9b58dcd30121b92f07e94cfa0
Daniel Gerrald <daniel.gerrald@icloud.com>
2020-10-09 13:00:42 -0400

    edit routes and carousel

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,112 +1,61 @@
 import React, { Component } from "react";
-import Backpack from "../../components/Inventory/backpack";
-import Shoe from "../../components/Inventory/shoe";
 import "../../components/App/App.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
-import Slider from "react-slick";
 import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
 import CardDeck from "react-bootstrap/CardDeck";
-import CardGroup from "react-bootstrap/CardGroup";
+import Carousel from "react-bootstrap/Carousel";
+
 
 class HomePage extends Component {
   render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      adaptiveHeight: true,
-      centerMode: true,
-    };
     return (
       <>
-        <Row>
-          <Col sm={4}>
-            <Card style={{ width: "18rem" }} id="navcolumn">
-              <Card.Header>Featured</Card.Header>
-              <ListGroup variant="flush">
-                <ListGroup.Item>Backpacks</ListGroup.Item>
-                <ListGroup.Item>Shoes</ListGroup.Item>
-                <ListGroup.Item>Tech</ListGroup.Item>
-                <ListGroup.Item>Clothes</ListGroup.Item>
-              </ListGroup>
-            </Card>
-          </Col>
-          <Col lg={3}>
-            <Slider {...settings} id="slider">
-              <div>
-                <img src={"../images/burberrybp.png"} />
-              </div>
-              <div>
-                <h1>2</h1>
-              </div>
-              <div>
-                <h1>3</h1>
-              </div>
-              <div>
-                <h1>4</h1>
-              </div>
-              <div>
-                <h1>5</h1>
-              </div>
-              <div>
-                <h1>6</h1>
-              </div>
-            </Slider>
-          </Col>
-          <Row></Row>
-          <CardDeck id="carddeck">
-            <Card class="card">
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This card has supporting text below as a natural lead-in to
-                  additional content.{" "}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This card has even longer
-                  content than the first to show that equal height action.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </CardDeck>
-        </Row>
+        <Col lg={3}>
+          <Carousel >
+            <Carousel.Item interval={1000}>
+              <img
+                className="d-block w-100"
+                src={"../images/leather.png"}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+              <img
+                className="d-block w-100"
+                src={"../images/AF1.png"}
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={"../images/burberrybp.png"}
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+
         <CardDeck id="carddeck">
-          <Card class="card">
-            <Card.Img variant="top" src="holder.js/100px160" />
+          <Card>
+            <Card.Img variant="top" src={"../images/burberrybp.png"} />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
@@ -116,34 +65,7 @@ class HomePage extends Component {
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src={"imgpath"} />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to
-                additional content.{" "}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small ClassName="text-muted">Last updated 3 mins ago</small>
             </Card.Footer>
           </Card>
         </CardDeck>
