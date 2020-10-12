1dc7b15f76f3aa6b561adf1d2f4bfbeab555a85e
StevenM404 <67655910+StevenM404@users.noreply.github.com>
2020-10-12 11:47:12 -0400

    added some chema

--- server/models/product.js
+++ server/models/product.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
+const { string, number } = require('yup');
 const Schema = mongoose.Schema;
 
 const ProductsSchema = new Schema({
-
-
-
-    
+    image: {},
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
 })
 
 let Product = mongoose.model("Product", ProductsSchema);
