f8fcc4b0bd493823d14562045bee2eb6db39d193
Adrian Diaz <adrian93eh3@gmail.com>
2020-10-13 14:11:44 -0400

    adding image urls to backpack/clothing

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -14,168 +14,168 @@ mongoose.connect(
 const productSeed = [
 
         {
-            "image": "burberrybp.png",
+            "image": "https://img.ssensemedia.com/image/upload/b_white/c_scale,h_820/f_auto,dpr_1.0/202376M166012_1.jpg",
             "name": "Burberry Checkered Bag",
             "price": 990.00,
             "category": "Backpack",
             "description": "This is a luxurious backpack made by none other than Burberry."
         },
         {
-            "image": "coachbp.png",
+            "image": "https://img1.cohimg.net/is/image/Coach/89931_qbcah_a0?fmt=jpg&wid=680&hei=885&bgc=f0f0f0&fit=vfit&qlt=75",
             "name": "Coach Large Ranger Backpack",
             "price": 250.00,
             "category": "Backpack",
             "description": "Large leather bag with many storage options, including a inside laptop sleeve, multifunction pockets, and closes with drawstring and clips."
         },
         {
-            "image": "guccibp.png",
+            "image": "https://img.ssensemedia.com/image/upload/b_white/c_scale,h_820/f_auto,dpr_1.0/202451M166261_1.jpg",
             "name": "Gucci Black Backpack",
             "price": 1980.00,
             "category": "Backpack",
             "description": "The GG motif in a distinct black/grey combination is further enriched with Web straps and black leather trims. Made in soft GG Supreme, a softer version of GG Supreme canvas, crafted from a coated microfiber fabric with the GG motif."
         },
         {
-            "image": "nikewheatbp.png",
+            "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQHqjGklhIC-tmrCFe4Ij1lU47xXJIfzcp6zwB8SxFgYgx5EqDdDYQX-2xpj9CwptYejl0l7tZMhfzc3l_fv1ovkdbQjfqsy-ZuTFd8fwMBywciUCBwfb0g&usqp=CAY",
             "name": "Nike Wheat Backpack",
             "price": 75.00,
             "category": "Backpack",
             "description": "The Nike Utility Speed Backpack keeps your gear close, secure and organized when commuting to and from training sessions. Cushioned straps give you comfort on the go, and the pack opens flat for easy access to must-have items."
         },
         {
-            "image": "ospreybp.png",
+            "image": "https://www.rei.com/media/c871bd1b-a4a7-4a27-b63e-e7d127368e12?size=784x588",
             "name": "Atmos AG 50",
             "price": 240.00,
             "category": "Backpack",
             "description": "The Atmos™ AG 50 is the best-fitting, most ventilated and comfortable pack ever made for traditional backpacking trips lasting a weekend or more. The innovative AntiGravity suspension makes it feel like you're carrying less weight and seamlessly wraps around your body, which means you'll enjoy every mile more than ever. From easy to use stretch mesh side pockets, Stow-on-the-Go trekking pole attachment, and FlapJacket for lidless use, the Atmos™ AG 50 is still the top contender. Backed by our All Mighty Guarantee, forever."
         },
         {
-            "image": "saintlaurentbp.png",
-            "name": "City Backpack in nylon canvas and leather",
+            "image": "https://img.ssensemedia.com/image/upload/b_white/c_scale,h_820/f_auto,dpr_1.0/202418M166423_1.jpg",
+            "name": "Saint Laurent Black Bag",
             "price": 860.00,
             "category": "Backpack",
             "description": "Backpack with leather trimmed Saint Laurent printed from pocket, Saint Laurent signature embossed exterior zip pocket and interior key clip, cell phone pocket, zip compartment and padded slot pocket."
         },
         {
-            "image": "supremeblackbp.png",
-            "name": "Supreme SS20 black",
+            "image": "https://stockx.imgix.net/products/streetwear/Supreme-Backpack-SS20-Black.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&trim=color&updated_at=1582320818&w=1000",
+            "name": "Supreme SS20 Black",
             "price": 225.00,
             "category": "Backpack",
-            "description": "Multi pockety bag with adjustable straps and two side pouches for watter bottels the outer most pocket is a see throught mesh."
+            "description": "Multi pocket bag with adjustable straps and two side pouches for water bottles the outer most pocket is a see throught mesh."
         },
         {
-            "image": "supremeredbp.png",
-            "name": "Supreme SS20 red",
+            "image": "https://stockx.imgix.net/products/streetwear/Supreme-Backpack-SS20-Red.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&trim=color&updated_at=1582233624&w=1000",
+            "name": "Supreme SS20 Red",
             "price": 225.00,
             "category": "Backpack",
-            "description": "Multi pockety bag with adjustable straps and two side pouches for watter bottels the outer most pocket is a see throught mesh."
+            "description": "Multi pocket bag with adjustable straps and two side pouches for water bottles the outer most pocket is a see throught mesh."
         },
         {
-            "image": "leather.png",
-            "name": "Crazy Horse Leather Backpack Yukon Bags",
+            "image": "https://cdn.shopify.com/s/files/1/2880/2774/products/TanHarperBackpack1_1800x1800.jpg?v=1591979771",
+            "name": "Harper Leather Bag",
             "price": 269.00,
             "category": "Backpack",
-            "description": "Crazy Horse Leather Backpack provides amazing durability along with giving off a stylish look. It has been carefully made with an embossing technique and from an excellent quality crazy horse leather. This bag acquires an antique, unique look with time since it is made of a natural material. Apart from being aesthetically pleasing, this backpack provides plenty of space in which you can easily store your essentials.This bag is also quite strong and sturdy and can withstand tough environments. The thoughtfully-designed shoulder straps distribute the weight evenly so that your shoulders feel relaxed and comfortable. You can also adjust the shoulder straps according to your liking. Crazy Horse Leather Backpack is a perfect choice for school, work, traveling, camping, or hiking etc. Its grab handle is lined with leather so you can easily and comfortably carry it around too."
+            "description": "Harper's Crazy Horse Leather Backpack provides amazing durability along with giving off a stylish look. It has been carefully made with an embossing technique and from an excellent quality crazy horse leather. This bag acquires an antique, unique look with time since it is made of a natural material. Apart from being aesthetically pleasing, this backpack provides plenty of space in which you can easily store your essentials.This bag is also quite strong and sturdy and can withstand tough environments. The thoughtfully-designed shoulder straps distribute the weight evenly so that your shoulders feel relaxed and comfortable. You can also adjust the shoulder straps according to your liking. Harper's Crazy Horse Leather Backpack is a perfect choice for school, work, traveling, camping, or hiking etc. Its grab handle is lined with leather so you can easily and comfortably carry it around too."
         },
         {
-            "image": "balenciagabp.png",
-            "name": "Wheel Small Backpack Balenciaga",
+            "image": "https://www.balenciaga.com/66/45/45474478cq_14_a_f.jpg",
+            "name": "Wheel Small Backpack",
             "price": 950.00,
             "category": "Backpack",
-            "description": "This amall nylon bag that was made in italy comes with adjustable shoulder straps, and three different pockets."
+            "description": "This small nylon bag that was made in Italy by the ever famous Balenciaga brand comes with adjustable shoulder straps."
         },
         {
-            "image": "coachbp2.png",
+            "image": "https://img1.cohimg.net/is/image/Coach/89897_qbm2_a0?fmt=jpg&wid=680&hei=885&bgc=f0f0f0&fit=vfit&qlt=75",
             "name": "Hudson Backpack with Hore and Carriage Print",
             "price": 289.00,
             "category": "Backpack",
-            "description": "Printed Coated canvas and natural pebble leather, the bag has multipule poutches for your phone or laptop."
+            "description": "Printed Coated canvas and natural pebble leather, the bag has multiple pouches for your phone or laptop."
         },
         {
-            "image": "coachbp3.png",
+            "image": "https://img1.cohimg.net/is/image/Coach/3001_qboh0_a0?fmt=jpg&wid=680&hei=885&bgc=f0f0f0&fit=vfit&qlt=75",
             "name": "West Backpack In Signature Canvas With Varsity Stripe",
             "price": 249.00,
             "category": "Backpack",
             "description": "Signature coated canvas and smooth calf leather, has two seperate compartments with zip closures, and adjustable shoulder straps."
         },
         {
-            "image": "HRF1.png",
+            "image": "https://a248.e.akamai.net/f/248/9086/10h/s7d2.scene7.com/is/image/FoxRacing/25956248_1?$dw_detn1$&wid=800&hei=800",
             "name": "Fox Racing Men's Honda Pullover",
             "price": 85.00,
             "category": "Clothing",
             "description": "This hoodie is made out of 80% cotton / 20% Polyester, is also part of the Fox X Honda collection"
         },
         {
-            "image": "HUFMultiF.png",
-            "name": "Wave Anorak Jacket",
+            "image": "https://media.hufworldwide.com/media/catalog/product/cache/image/1268x1172/e9c3970ab036de70892d86c6d221abfe/W/A/WAVE-ANORAK-JACKET_HOT-PINK_JK00229_HOTPK_01.jpg",
+            "name": "HUF Multicolored Jacket",
             "price": 85.00,
             "category": "Clothing",
             "description": "Inspired by retro outdoors wear, the Wave Anorak Jacket is made of durable nylon treated with a water-repellent coating and is equipped with a half-zip opening, two handwarmer pockets, and a high-collar hood for extra protection. Constructed with color-blocked paneling for a throwback vibe, the jacket features HUF's 'Peak' logo in a rubber patch on the front pocket, and in a reflective foil-print across the back."
         },
         {
-            "image": "HUFCamoF.png",
-            "name": "Network Lightweight Jacket",
+            "image": "https://media.hufworldwide.com/media/catalog/product/cache/image/1268x1172/e9c3970ab036de70892d86c6d221abfe/N/E/NETWORK-LIGHTWEIGHT-JACKET_REALTREE-BLACK_JK00248_RTBLK_01.jpg",
+            "name": "HUF Camo Jacket",
             "price": 110.00,
             "category": "Clothing",
             "description": "HUF teams up with renowned outdoors wear-specialist Realtree® on the Network Lightweight Jacket, inspired by 90s tactical gear. Featuring Realtree’s® signature 3D camouflage, the jacket is made of water-repellent nylon with a mesh overlay for a retro aesthetic. Cut with a full-zip front and spacious high-collar hood to protect against heavy weather, the piece is finished with two handwarmer pockets, a rubber Triple Triangle logo at left chest, and HUF’s ‘Peak’ logo at back."
         },
         {
-            "image": "BrownF.png",
-            "name": "Hooded Puffer Jacket",
+            "image": "https://s7d5.scene7.com/is/image/UrbanOutfitters/56539760_023_b?$xlarge$&fit=constrain&qlt=80&wid=683",
+            "name": "North Face Tan Puffer Jacket",
             "price": 70.00,
             "category": "Clothing",
             "description": "Padded jacket in windproof, water-repellent fabric. Lined, detachable hood with drawstring and faux fur trim. Zipper at front and wind flap with snap fasteners. Fleece-lined handwarmer pockets, patch pockets with flap and snap fasteners, and one inner pocket with hook-loop fastener. Ribbing at cuffs and hem. Lined. Polyester fill."
         },
         {
-            "image": "ItachiF.png",
+            "image": "https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/Primitive-x-Naruto-Crows-Burgundy-Coaches-Jacket-_337361-front-US.jpg",
             "name": "Primitive x Naruto Crows Burgundy Coaches Jacket",
             "price": 75.00,
             "category": "Clothing",
             "description": "Layer up in iconic anime swag with the Crows burgundy coaches jacket from Primitive x Naruto. This burgundy coaches jacket features a classic foldover collar, button-up placket, and collaborative Primitive and Naruto graphics screen-printed at the left chest and back to complete the design."
         },
         {
-            "image": "FakeFriendsF.png",
+            "image": "https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/Broken-Promises-We-Ain-t-Homies-Black-Hooded-Coaches-Jacket-_315315-front-US.jpg",
             "name": "Broken Promises We Ain't Homies Black Hooded Coaches Jacket",
             "price": 70.00,
             "category": "Clothing",
             "description": "Grab an eye-catching new layer for the cooler weather with the We Ain't Homies black jacket from Broken Promises. This hooded jacket comes in black and features embroidered text at the left chest in white that reads We Ain't Homies while the back displays large white screen-printed text that reads fake friends stab you in the back, real friends stab you in the heart. A full snap button placket with custom BP hardware, adjustable drawstring hood and bottom hem, along with two hand pockets offer a classic silhouette, while the light grey jersey lining offers a comfortable fit and feel"
         },
         {
-            "image": "VansF.png",
+            "image": "https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/Vans-Barbed-Floral-White-Hoodie-_167121-front-CA.jpg",
             "name": "Vans Barbed Floral White Hoodie",
             "price": 55.00,
             "category": "Clothing",
             "description": "Spruce up your hoodie game with some fun floral flair in the Vans Barbed floral white hoodie. A mostly white colorway throughout is contrasted by screen-printed pink logo script on the left chest for colorful branding, while the floral, barbed wire patterns lining both sleeves add some serious fashion-forward flair. A fleece lined interior and relaxed fit keep this hoodie extremely comfortable and the adjustable drawstring hood along with kangaroo pouch pocket maintain a truly classic silhouette overall."
         },
         {
-            "image": "OldSoulF.png",
+            "image": "https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/Empyre-Fredia-Old-Soul-Black-%26-Purple-Tie-Dye-Hoodie-_330505-front-US.jpg",
             "name": "Empyre Fredia Old Soul Black & Purple Tie Dye Hoodie",
             "price": 35.00,
             "category": "Clothing",
             "description": "Your favorite hoodie is here to keep you cozy all year long. The Fredia Old Soul black and purple tie-dye hoodie from Empyre features a unique purple and black dye treatment throughout for a one-of-a-kind look, while the adjustable hood and kangaroo pocket is sure to keep you warm and comfy. Finished with wild graphics screen-printed on the back of a trippy skeleton with butterflies and mushrooms, along with text that reads old soul."
         },
         {
-            "image": "UnfollowF.png",
+            "image": "https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/Broken-Promises-Unfollow-Black-Tie-Dye-Hoodie-_335573-back-US.jpg",
             "name": "Broken Promises Unfollow Black Tie-Dye Hoodie",
             "price": 52.00,
             "category": "Clothing",
             "description": "Wear the Unfollow black tie-dye hoodie from Broken Promises as a reminder that it's okay to cut negative influences out of your life. This hooded sweatshirt comes in a black and grey tie-dye color and features a white graphic across the back and sleeves for additional impact."
         },
         {
-            "image": "LoveDrugF.png",
+            "image": "https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/Civil-Love-Is-A-Drug-Orchid-Hoodie-_336547-front-US.jpg",
             "name": "Civil Love Is A Drug Orchid Hoodie",
             "price": 65.00,
             "category": "Clothing",
             "description": "Throw some irresistible color into your collection with the Love Is A Drug orchid hoodie from Civil! This purple-pink hoodie features a large rose graphic at the center chest, surrounded by Love is a drug text, and the Civil Regime Rebel Youth text on the left sleeve gives it a ton of killer branding."
         },
         {
-            "image": "PinkF.png",
+            "image": "https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/Dravus-Dierdra-Pink-Hoodie-_330503-back-US.jpg",
             "name": "Dravus Dierdra Pink Hoodie",
             "price": 35.00,
             "category": "Clothing",
             "description": "Succulents are all the rage and the Dierdra pink hoodie from Dravus is a stylish way to get in on the fun. Several succulent graphics across the back are complemented by the Life would succ without you, let's stick together text that surrounds them, for a bit of piercing humor. Finished off with the flower graphic at the left chest, the Dierdra hoodie is sure to give your look some extra botanical pizzazz."
         },
         {
-            "image": "LoserF.png",
+            "image": "https://scene7.zumiez.com/is/image/zumiez/product_main_medium_2x/JV-by-Jac-Vanek-Loser-Purple%2C-Yellow%2C-%26-Blue-Tie-Dye-Hoodie-_336819-front-US.jpg",
             "name": "JV by Jac Vanek Loser Purple, Yellow, & Blue Tie Dye Hoodie",
             "price": 45.00,
             "category": "Clothing",
